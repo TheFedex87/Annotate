baba1844c2f36d66fa6a5b77e709bf3d1a78b57a
Federico Creti <federico.creti87@gmail.com>
2024-01-20 14:51:42 +0100

    Gradle plugin and java version update

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,7 +12,7 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
-    implementation("com.android.tools.build:gradle:8.0.2")
+    implementation("com.android.tools.build:gradle:8.1.0")
 
     // Required to fix a missing dependency error
     implementation("com.squareup:javapoet:1.13.0")
