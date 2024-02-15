f6b60f3cb9e1906f9d0d95b249cd71627bace1b0
Federico Creti <federico.creti87@gmail.com>
2024-02-15 20:03:58 +0100

    Added DataStore depenendy and updated some dependencies

--- buildSrc/src/main/java/Kotlin.kt
+++ buildSrc/src/main/java/Kotlin.kt
@@ -1,4 +1,7 @@
 object Kotlin {
     // private const val version = "1.9.22"
     // const val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$version"
+
+    private const val immutableCollectionVersion = "0.3.7"
+    const val immutableList = "org.jetbrains.kotlinx:kotlinx-collections-immutable:$immutableCollectionVersion"
 }
--- buildSrc/src/main/java/Modules.kt
+++ buildSrc/src/main/java/Modules.kt
@@ -3,7 +3,9 @@ object Modules {
     const val coreUi = ":core_ui"
 
     const val notesDomain = ":notes:notes_domain"
+    const val notesData = ":notes:notes_data"
     const val notesPresentation = ":notes:notes_presentation"
+    const val notesUtils = ":notes:notes_utils"
 
     const val utils = ":utils"
 }
