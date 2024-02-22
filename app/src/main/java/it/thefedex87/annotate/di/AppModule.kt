1f83b171e3c1e3405f82611eb5163017ffe4a904
Federico Creti <federico.creti87@gmail.com>
2024-02-22 11:29:54 +0100

    Added notesPreferencesManager in AppModule DI

--- app/src/main/java/it/thefedex87/annotate/di/AppModule.kt
+++ app/src/main/java/it/thefedex87/annotate/di/AppModule.kt
@@ -1,14 +1,18 @@
 package it.thefedex87.annotate.di
 
 import android.app.Application
+import android.content.Context
 import androidx.room.Room
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import it.thefedex87.core.data.local.AnnotateDatabase
 import it.thefedex87.core.data.local.BlockNoteDao
+import it.thefedex87.notes_data.preferences.DefaultNotesPreferencesManager
 import it.thefedex87.notes_data.repository.NotesRepositoryImpl
+import it.thefedex87.notes_domain.preferences.NotesPreferencesManager
 import it.thefedex87.notes_domain.repository.NotesRepository
 import it.thefedex87.utils.Consts
 import javax.inject.Singleton
@@ -32,8 +36,17 @@ object AppModule {
 
     @Singleton
     @Provides
-    fun provideNotesRepository(blockNoteDao: BlockNoteDao): NotesRepository =
+    fun provideNotesPreferencesManager(@ApplicationContext context: Context): NotesPreferencesManager =
+        DefaultNotesPreferencesManager(context)
+
+    @Singleton
+    @Provides
+    fun provideNotesRepository(
+        blockNoteDao: BlockNoteDao,
+        notesPreferencesManager: NotesPreferencesManager
+    ): NotesRepository =
         NotesRepositoryImpl(
-            blockNoteDao = blockNoteDao
+            blockNoteDao = blockNoteDao,
+            notesPreferencesManager = notesPreferencesManager
         )
 }
