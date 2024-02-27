22bbe47794b301309d3f8c2e804aa8be8a91ad18
Federico Creti <federico.creti87@gmail.com>
2024-02-27 21:31:55 +0100

    Methods utils file

---
+++ utils/src/main/java/it/thefedex87/utils/Methods.kt
@@ -0,0 +1,17 @@
+package it.thefedex87.utils
+
+import java.io.Serializable
+import kotlin.Triple
+
+data class Quadruple<out A, out B, out C, out D>(
+    val first: A,
+    val second: B,
+    val third: C,
+    val fourth: D
+) : Serializable {
+
+    /**
+     * Returns string representation of the [Quadruple] including its [first], [second], [third] and [fourth] values.
+     */
+    override fun toString(): String = "($first, $second, $third, $fourth)"
+}
